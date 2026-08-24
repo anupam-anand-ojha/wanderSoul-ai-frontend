887601eb0249826e29d8c7304bfbe0699fdc1381
Anupam Anand <theanupamojha999@gmail.com>
2026-08-25 00:16:45 +0530

    add routes to destinations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+
+const App = () => {
   return (
-    <div><Navbar/></div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-export default App
+        <Route path="/explore" element={<Explore />} />
+
+        <Route path="/explore/bali" element={<Destination />} />
+        <Route path="/explore/kashmir" element={<Destination />} />
+        <Route path="/explore/santorini" element={<Destination />} />
+        <Route path="/explore/dubai" element={<Destination />} />
+        <Route path="/explore/manali" element={<Destination />} />
+        <Route path="/explore/kyoto" element={<Destination />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
