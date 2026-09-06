1c92fdf62dc9bcb8a3f612cf7df2ce62abca1a99
Anupam Anand <theanupamojha999@gmail.com>
2026-09-06 23:26:19 +0530

    update home for mobile device

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,15 +40,15 @@ const Home = () => {
         <Navbar />
 
         {/* Hero Content */}
-        <div className="relative z-10 flex min-h-screen items-center px-6 pt-20 md:px-10">
+        <div className="relative z-10 flex min-h-screen md:items-center px-6 pt-40 md:pt-20 md:px-10">
           <div className="mx-auto w-full max-w-7xl">
             <div className="max-w-3xl">
 
-              <div className="mb-6 inline-flex rounded-full border border-[#EE82EE]/40 bg-[#800080]/20 px-4 py-3 text-sm text-[#EE82EE] backdrop-blur-md">
+              <div className=" mb-18 md:mb-6 inline-flex rounded-full border border-[#EE82EE]/40 bg-[#800080]/20 px-4 py-3 text-sm text-[#EE82EE] backdrop-blur-md">
                 AI-powered travel planning
               </div>
 
-              <h1 className="text-5xl font-bold leading-[0.95] tracking-tight text-white sm:text-6xl md:text-7xl lg:text-8xl">
+              <h1 className=" text-6xl md:text-5xl font-bold leading-[0.95] tracking-tight text-white sm:text-6xl md:text-7xl lg:text-8xl">
                 Your next
                 <br />
 
